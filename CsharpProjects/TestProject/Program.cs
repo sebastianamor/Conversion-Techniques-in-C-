2128212cb4d9762cafc4c3e351683fad2d7463ae
sebastian lapt <smoreira11@hotmail.com>
2025-08-09 08:10:45 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,11 +1,17 @@
-﻿string value = "102";
-int result = 0;
-if (int.TryParse(value, out result))
-{
-   Console.WriteLine($"Measurement: {result}");
-}
-else
-{
-   Console.WriteLine("Unable to report the measurement.");
-}
-Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+﻿//string value = "102";
+//int result = 0;
+//if (int.TryParse(value, out result))
+//{
+ //  Console.WriteLine($"Measurement: {result}");
+//}
+//else
+//{
+  // Console.WriteLine("Unable to report the measurement.");
+//}
+//Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+
+Console.WriteLine("Message:  ");
+Console.WriteLine($"Total:");
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7383dc505fdbf83d71627824e131726d25e25d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9ab8f9f6ae2fcc5397c00359959e01433ac180")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
