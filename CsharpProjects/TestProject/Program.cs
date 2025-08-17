ef5de5863ec35ebb361e532ae9a21338c5ebbf63
sebastian lapt <smoreira11@hotmail.com>
2025-08-18 08:10:50 +0900

    add

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -12,6 +12,20 @@
 
 string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
+decimal total = 0m;
+string message = "";
 
-Console.WriteLine("Message:  ");
-Console.WriteLine($"Total:");
+foreach (var value in values)
+{
+    decimal number; // stores the TryParse "out" value
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    } else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9ab8f9f6ae2fcc5397c00359959e01433ac180")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2128212cb4d9762cafc4c3e351683fad2d7463ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
